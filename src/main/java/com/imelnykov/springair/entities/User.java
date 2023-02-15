3576ec5e6140d69cedea93dfc9dc5ea7faf69381
Illia Melnykov <91326814+ttdever@users.noreply.github.com>
2023-02-15 13:50:05 +0100

    Removed wildcard imports

--- src/main/java/com/imelnykov/springair/entities/User.java
+++ src/main/java/com/imelnykov/springair/entities/User.java
@@ -1,6 +1,12 @@
 package com.imelnykov.springair.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 
 import java.util.List;
 
